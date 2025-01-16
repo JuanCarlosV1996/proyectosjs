7f45eada1df851a3af50bfa8724d2470e115c269
juancarlosvinueza <vinuezajuan96@gmail.com>
2025-01-15 21:29:19 -0500

    Proyecto react cotizador

---
+++ COTIZADOR/cotizador-prestamos-react/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  const hola = "Hola Mundo"
+  return (
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+      {hola}
+    </div>
+  )
+}
+
+export default App
