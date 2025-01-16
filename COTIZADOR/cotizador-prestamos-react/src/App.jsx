afac740cb171c74e471054e69062ba422b2f134a
Juan Carlos Vinueza <vinuezajuan96@gmail.com>
2025-01-16 17:02:37 -0500

    uso de state

--- COTIZADOR/cotizador-prestamos-react/src/App.jsx
+++ COTIZADOR/cotizador-prestamos-react/src/App.jsx
@@ -1,9 +1,20 @@
+import { useState } from 'react'
+import Header from "./components/Header"
+
 function App() {
 
-  const hola = "Hola Mundo"
+  const [cantidad, setCantidad] = useState(10000);
+
+  console.log(cantidad);
+
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
-      {hola}
+      <Header />
+
+      <input 
+        type="range" 
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+      />
     </div>
   )
 }
