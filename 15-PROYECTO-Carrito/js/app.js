aae41cfe9d9eb837053ff1677a6c967c4072a53a
juancarlosvinueza <vinuezajuan96@gmail.com>
2025-01-15 19:57:10 -0500

    Siguiendo

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -15,8 +15,24 @@ function cargarEventListeners () {
 function agregarCurso(e) {
     e.preventDefault();
     if(e.target.classList.contains('agregar-carrito')){
-        console.log(e.target)
-        console.log("prueba")
-        console.log("prueba 2")
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+
+        leerDatosCurso(cursoSeleccionado);
     }
+}
+
+// LEE EL CONTENIDO DEL HTML AL QUE LE DIMO CLICK Y EXTRAE LA INFORMACIÓN DEL CURSO
+function leerDatosCurso(curso) {
+    console.log(curso);
+
+    //Crear un objeto con el contenido del curso actual
+    const infoCruso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+
+    console.log(infoCruso);
 }
